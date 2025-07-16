5672a2666e10dd5aabedcac24d1d0e2f5237f437
Mohamed M <mm6289350@gmail.com>
2025-07-16 16:09:02 +0530

    Changed folder structure

---
+++ src/test/Java/Collection/hashSet.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class hashSet {
+    public static void main(String[] args) {
+        HashSet <String> set = new HashSet<String>();
+        set.add("Capo");
+        set.add("Capo");
+        set.add("wizz");
+        set.add("CZ");
+        //Ignores the duplicate elements and prints unordered
+        for(String str:set){
+            System.out.println(str);
+        }
+    }
+}
